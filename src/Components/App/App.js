e310c3fd663efbe5c4e7afa89042c0131cd422e6
Sam Baldwin <sambaldwin92@gmail.com>
2023-04-29 12:13:23 +0100

    Added fade to main message component container

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography, Fade } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import React from "react";
 
 const theme = createTheme({
   typography: {
@@ -17,24 +18,39 @@ const theme = createTheme({
 });
 
 function App() {
+  const [showBox, setShowBox] = React.useState(false);
+
+  React.useEffect(() => {
+    setShowBox(true);
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
-      <Container
-        maxWidth="xl"
-        sx={{
-          backgroundColor: "primary.main",
-          height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Box>
-          <Typography variant="h1" sx={{backgroundColor: "secondary.main", borderRadius: "0.5rem", p: "1rem"}}>
-            Hello World!
-          </Typography>
-        </Box>
-      </Container>
+      <Fade in={showBox} timeout={3000}>
+        <Container
+          maxWidth="xl"
+          sx={{
+            backgroundColor: "primary.main",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Box>
+            <Typography
+              variant="h1"
+              sx={{
+                backgroundColor: "secondary.main",
+                borderRadius: "0.5rem",
+                p: "1rem",
+              }}
+            >
+              Hello World!
+            </Typography>
+          </Box>
+        </Container>
+      </Fade>
     </ThemeProvider>
   );
 }
