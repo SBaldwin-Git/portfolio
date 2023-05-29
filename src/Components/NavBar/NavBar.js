c6600b81512ecfdb6edd8ce87a4a1d31691954bb
Sam Baldwin <sambaldwin92@gmail.com>
2023-05-29 21:01:48 +0100

    Begun work on About page, added multiple style sheets and also removed underline styling from navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar() {
   return (
     <AppBar position="static" sx={{ height: "0.2em" }}>
       <Toolbar sx={{ display: "flex", justifyContent: "flex-end", gap: "2em" }}>
         <Typography variant="h6" sx={{ fontSize: "1rem" }}>
-          <Link to="/">Home</Link>
+          <Link to="/">
+            Home
+          </Link>
         </Typography>
         <Typography variant="h6" sx={{ fontSize: "1rem" }}>
-          <Link to="/projects">Projects</Link>
+          <Link to="/projects" >
+            Projects
+          </Link>
         </Typography>
-        <Typography variant="h6" sx={{ fontSize: "1rem" }}>
-          <Link to="/about">About</Link>
+        <Typography variant="h6" sx={{ fontSize: "1rem"}}>
+          <Link to="/about" >
+            About
+          </Link>
         </Typography>
       </Toolbar>
     </AppBar>
