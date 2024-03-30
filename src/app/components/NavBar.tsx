94e9a7b24d0e1bcacac52d6def03bbf5d0ffd845
Sam Baldwin <sambaldwin92@gmail.com>
2024-03-30 15:50:13 +0000

    Refactored navbar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -3,22 +3,23 @@ import React from "react";
 function NavBar() {
   return (
     <header className="text-ivory flex justify-between w-full">
+      <div className="nameContainer">
+        <h1 className="text-ivory font-bold text-[2.5rem]">Sam Baldwin</h1>
+      </div>
+
       <nav className="flex flex-row">
-        <ul>
+        <ul className="flex gap-8 text-[2rem]">
           <li>
-            <a href="#home">Home</a>
+            <a href="#about">About</a>
           </li>
           <li>
             <a href="#projects">Projects</a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
           </li>
         </ul>
       </nav>
-      <div className="gitHubContainer">
-        <a href="https://github.com/SBaldwin-Git">SBaldwin-Git</a>
-      </div>
     </header>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center h-full w-8/12 mx-auto">
+    <main className="flex flex-col items-center justify-center h-full w-8/12 mx-auto mt-10">
       <NavBar />
       <article>
         <h1 className="text-ivory font-bold text-[10rem] ">Hello World</h1>
