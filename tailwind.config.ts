480523f605b22590f11f89c24634ff10921d5d6f
Sam Baldwin <sambaldwin92@gmail.com>
2024-04-07 12:54:08 +0100

    Added more colours for testing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,10 +10,16 @@ const config: Config = {
     extend: {
       colors: {
         "prussian-blue": "#132637ff",
-        "ivory": "#fffee9ff",
+        ivory: "#fffee9ff",
         "raisin-black": "#2b2839ff",
         "light-coral": "#e67470ff",
         "davys-gray": "#544e57ff",
+        "baby-powder": "#F8FCFB",
+        "mint-cream": "#F5FFFA",
+        "lavender-blush": "#FFF5FA",
+        "red-crayola": "#EE4266",
+        "french-grey": "#B3B7C6",
+        "onyx": "#3E3F4B",
       },
     },
   },
