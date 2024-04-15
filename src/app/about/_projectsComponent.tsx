8f1ac4c7ab9813eeb923efdd5c04eb8cd3366a40
Sam Baldwin <sambaldwin92@gmail.com>
2024-04-15 16:33:45 +0100

    Added projects component file to about page

---
+++ src/app/about/_projectsComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+
+function projectsComponent() {
+  return (
+    <div>projectsComponent</div>
+  )
+}
+
+export default projectsComponent
