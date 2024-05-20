b2a6741d5f1fdb720d0bd4ace2663b8022998432
Sam Baldwin <sambaldwin92@gmail.com>
2024-05-20 12:44:43 +0100

    Added github icons and text to button

--- src/app/about/components/_ButtonRow.tsx
+++ src/app/about/components/_ButtonRow.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Image from "next/image";
 
 function ButtonRow() {
   return (
     <div className="flex gap-6">
-      <button className="bg-red-crayola text-raisin-black py-2 px-4 rounded border-4 border-raisin-black font-bold w-60 h-20 text-3xl">
+      <button className="bg-red-crayola text-raisin-black py-2 px-4 rounded-lg border-4 border-raisin-black font-bold w-60 h-20 text-4xl">
         Projects
       </button>
-      <button className="bg-red-crayola text-raisin-black py-2 px-4 rounded border-4 border-raisin-black font-bold w-60 h-20 text-3xl">
-        Contact Me
+      <button className="bg-raisin-black text-sea-salt py-2 px-4 rounded-lg border-4 border-raisin-black font-bold w-60 h-20 text-3xl
+      flex justify-center items-center gap-2">
+        <Image src='/images/GitHub_Logo_White.png' alt="GitHub Logo" width={120} height={90} />
+        <Image src='/images/github-mark-white.png' alt="GitHub Cat" width={40} height={40}/>
       </button>
     </div>
   );
