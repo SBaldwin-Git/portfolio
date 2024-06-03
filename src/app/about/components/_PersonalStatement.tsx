6ec51a0ff074b4f70d1abfce9edd832b76f425c0
Sam Baldwin <sambaldwin92@gmail.com>
2024-06-03 12:10:12 +0100

    updated bio

--- src/app/about/components/_PersonalStatement.tsx
+++ src/app/about/components/_PersonalStatement.tsx
@@ -6,8 +6,9 @@ function PersonalStatement() {
     <div>
       <h2 className="text-2xl font-bold opacity-70">
         I&apos;m a software developer currently based in Buckinghamshire,
-        looking an entry level role where I can grow and work with amazing
-        people while developing my awful sense of humour.
+        looking for an entry level role where I can grow and work with amazing
+        people while developing my awful sense of humour. My favourite part of
+        developing software are the people I get to meet and learn from!
       </h2>
     </div>
   );
