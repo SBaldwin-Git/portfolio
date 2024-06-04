4a5e913f0f2575b8412806ae009829cad5498f33
Sam Baldwin <sambaldwin92@gmail.com>
2024-06-04 17:40:54 +0100

    Completed static design of mobile navbar

--- src/app/components/_NavBar.tsx
+++ src/app/components/_NavBar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import { useState } from "react";
 import Hamburger from "hamburger-react";
@@ -9,9 +9,8 @@ function NavBar() {
   return (
     <header className="sticky top-0 text-raisin-black flex lg:justify-end w-full lg:w-screen mb-12 pt-4 pr-6 z-50">
       <div className="flex items-center md:hidden">
-        <Hamburger rounded size={48} toggled={isOpen} toggle={setOpen} />
+        <Hamburger rounded size={40} toggled={isOpen} toggle={setOpen} />
       </div>
-
       <ul className="hidden lg:flex lg:gap-14 text-4xl">
         <li>
           <a href="#about">About</a>
@@ -23,6 +22,28 @@ function NavBar() {
           <a href="#contact">Contact</a>
         </li>
       </ul>
+
+      {/* Side Panel Mobile Menu */}
+      <div
+        className={`${
+          isOpen ? "fixed" : "hidden"
+        } top-0 left-0 z-50 h-full w-1/2 shadow-lg bg-sea-salt transform transition-transform ease-out duration-200 p-4 gap-2.5 flex justify-between`}
+      >
+        <ul className="flex flex-col lg:gap-14 text-4xl">
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <div>
+          <Hamburger rounded size={32} toggled={isOpen} toggle={setOpen} />
+        </div>
+      </div>
     </header>
   );
 }
