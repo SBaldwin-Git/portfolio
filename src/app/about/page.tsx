8448de0b0fe7eae79897ae8e49d74e7aa3b3805d
Sam Baldwin <sambaldwin92@gmail.com>
2024-06-05 11:09:06 +0100

    bio gap change

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { RoughNotationGroup } from "react-rough-notation";
-import Intro from "./components/_Intro";
 import NameHeader from "./components/_NameHeader";
 import TechStack from "./components/_TechStack";
 import ButtonRow from "./components/_ButtonRow";
@@ -8,7 +7,7 @@ import PersonalStatement from "./components/_PersonalStatement";
 
 function About() {
   return (
-    <article className="text-raisin-black flex flex-col gap-12 sm:gap-24 mr-auto w-full">
+    <article className="text-raisin-black flex flex-col gap-12 sm:gap-24 lg:gap-28 mr-auto w-full">
       <RoughNotationGroup show={true}>
         <NameHeader />
         <div className="flex flex-col gap-10">
