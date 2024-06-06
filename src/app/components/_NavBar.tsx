564033e64002963543cc9b83305eef86ac14da5e
Sam Baldwin <sambaldwin92@gmail.com>
2024-06-06 17:08:37 +0100

    spelling

--- src/app/components/_NavBar.tsx
+++ src/app/components/_NavBar.tsx
@@ -9,7 +9,7 @@ function NavBar() {
 
   return (
     <header className="sticky top-0 text-raisin-black flex xl:justify-end w-full lg:w-screen mb-5 xl:mb-12 pt-2 xl:p-4 xl:pr-6 z-50">
-      {/* Mobile Burger Menu - Hidden on Desktop Displau  */}
+      {/* Mobile Burger Menu - Hidden on Desktop Display  */}
       <div className="flex items-center xl:hidden justify-between w-full">
         <Hamburger rounded size={42} toggled={isOpen} toggle={setOpen} />
         <button className="flex justify-center items-center rounded-full bg-raisin-black hover:bg-bright-raisin-black w-28 h-12">
